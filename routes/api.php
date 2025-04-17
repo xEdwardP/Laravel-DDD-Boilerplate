6a3737a2720a3433bee2dadb6a503d1fe2c8df5d
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2025-04-17 17:57:56 -0600

    Boilerplate preparation

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('platform_purchase')->group(base_path('src/platform/purchase/infrastructure/routes/api.php'));
---
+++ src/platform/purchase/infrastructure/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+//use Src\platform\purchase\infrastructure\controllers\ExampleGETController;
+
+// Simpele route example
+// Route::get('/', [ExampleGETController::class, 'index']);
+
+//Authenticathed route example
+// Route::middleware(['auth:sanctum','activitylog'])->get('/', [ExampleGETController::class, 'index']);
